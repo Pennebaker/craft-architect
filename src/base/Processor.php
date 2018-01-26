e5275e467c9ce2cb008db4e91f9e3b1c8f119b29
Spenser Hannon <spenson66@gmail.com>
2018-01-26 13:35:33 -0600

    Implement base Processor class.

---
+++ src/base/Processor.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Architect plugin for Craft CMS 3.x
+ *
+ * CraftCMS plugin to generate content models from JSON data.
+ *
+ * @link      https://pennebaker.com
+ * @copyright Copyright (c) 2018 Pennebaker
+ */
+
+namespace pennebaker\architect\base;
+
+use Craft;
+
+/**
+ * Processor defines the common interface to be implemented by plugin classes.
+ *
+ * @author    Pennebaker
+ * @package   Architect
+ * @since     2.0.0
+ */
+abstract class Processor implements ProcessorInterface
+{
+}
