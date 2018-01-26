ca427baeeda93626d75dab125e18058659cd4b0a
Spenser Hannon <spenson66@gmail.com>
2018-01-26 13:27:35 -0600

    Update trace, info, warning and error functions to accept params

--- src/Architect.php
+++ src/Architect.php
@@ -105,32 +105,7 @@ class Architect extends Plugin
             }
         );
 
-/**
- * Logging in Craft involves using one of the following methods:
- *
- * Craft::trace(): record a message to trace how a piece of code runs. This is mainly for development use.
- * Craft::info(): record a message that conveys some useful information.
- * Craft::warning(): record a warning message that indicates something unexpected has happened.
- * Craft::error(): record a fatal error that should be investigated as soon as possible.
- *
- * Unless `devMode` is on, only Craft::warning() & Craft::error() will log to `craft/storage/logs/web.log`
- *
- * It's recommended that you pass in the magic constant `__METHOD__` as the second parameter, which sets
- * the category to the method (prefixed with the fully qualified class name) where the constant appears.
- *
- * To enable the Yii debug toolbar, go to your user account in the AdminCP and check the
- * [] Show the debug toolbar on the front end & [] Show the debug toolbar on the Control Panel
- *
- * http://www.yiiframework.com/doc-2.0/guide-runtime-logging.html
- */
-
-        Craft::info(
-            Architect::t(
-                '{name} plugin loaded',
-                ['name' => $this->name]
-            ),
-            __METHOD__
-        );
+        self::info('{name} plugin loaded', ['name' => $this->name]);
     }
 
     /**
@@ -144,24 +119,24 @@ class Architect extends Plugin
         return Craft::t('architect', $message, $params);
     }
 
-    public static function trace($message)
+    public static function trace($message, array $params = [])
     {
-        Craft::trace(self::t($message), __METHOD__);
+        Craft::trace(self::t($message, $params), __METHOD__);
     }
 
-    public static function info($message)
+    public static function info($message, array $params = [])
     {
-        Craft::info(self::t($message), __METHOD__);
+        Craft::info(self::t($message, $params), __METHOD__);
     }
 
-    public static function warning($message)
+    public static function warning($message, array $params = [])
     {
-        Craft::warning(self::t($message), __METHOD__);
+        Craft::warning(self::t($message, $params), __METHOD__);
     }
 
-    public static function error($message)
+    public static function error($message, array $params = [])
     {
-        Craft::error(self::t($message), __METHOD__);
+        Craft::error(self::t($message, $params), __METHOD__);
     }
 
     // Protected Methods
