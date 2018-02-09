46ca3d8375295d7cf7f264901d63b5b05842a3ad
Spenser Hannon <spenson66@gmail.com>
2018-02-09 16:24:46 -0600

    Implement export landing page with everything listed available to export.

---
+++ src/variables/ArchitectVariable.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Architect plugin for Craft CMS 3.x
+ *
+ * CraftCMS plugin to generate content models from JSON data.
+ *
+ * @link      https://pennebaker.com
+ * @copyright Copyright (c) 2018 Pennebaker
+ */
+
+namespace pennebaker\architect\variables;
+
+use craft\elements\User;
+
+class ArchitectVariable
+{
+    // Public Methods
+    // =========================================================================
+    /**
+     * @return array
+     */
+    public function getAllUsers()
+    {
+        return User::findAll();
+    }
+}
