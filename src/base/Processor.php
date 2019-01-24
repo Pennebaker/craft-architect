11cbcd7dd22cd8ca102a5dea0fdd74d3b203c730
Spenser Hannon <spenson66@gmail.com>
2019-01-24 13:51:25 -0600

    Fix permissions mapping key references. #36

--- src/base/Processor.php
+++ src/base/Processor.php
@@ -11,7 +11,8 @@
 namespace pennebaker\architect\base;
 
 use Craft;
-use craft\base\VolumeInterface;
+use craft\db\Query;
+use craft\db\Table;
 use craft\errors\SiteNotFoundException;
 use craft\models\FieldLayout;
 use craft\base\Field;
@@ -306,330 +307,6 @@ abstract class Processor implements ProcessorInterface
     }
 
     /**
-     * @param array|string $sources
-     * @param string $prefix
-     */
-    public function mapVolumeSources(&$sources, $prefix = '')
-    {
-        if (is_array($sources)) {
-            foreach ($sources as $k => $sourceHandle) {
-                $source = Craft::$app->volumes->getVolumeByHandle($sourceHandle);
-                if ($source) {
-                    $sources[$k] = $prefix . $source->id;
-                } else {
-                    unset($sources[$k]);
-                }
-            }
-        } else if (is_string($sources)) {
-            $source = Craft::$app->volumes->getVolumeByHandle($sources);
-            if ($source) {
-                $sources = $prefix . $source->id;
-            } else {
-                $sources = '*';
-            }
-        } else {
-            $sources = '*';
-        }
-    }
-
-    /**
-     * @param array|string $sources
-     * @param string $prefix
-     */
-    public function unmapVolumeSources(&$sources, $prefix = '')
-    {
-        if (is_array($sources)) {
-            foreach ($sources as $k => $sourceRef) {
-                $sourceId = substr($sourceRef, strlen($prefix));
-                $source = Craft::$app->volumes->getVolumeById((int) $sourceId);
-                if ($source) {
-                    $sources[$k] = $source->handle;
-                } else {
-                    unset($sources[$k]);
-                }
-            }
-        } else if (isset($sources) && $sources !== '*' && $sources !== '') {
-            $sourceId = substr($sources, strlen($prefix));
-            $source = Craft::$app->volumes->getVolumeById((int) $sourceId);
-            if ($source) {
-                $sources = $source->handle;
-            } else {
-                $sources = '*';
-            }
-        }
-    }
-
-    /**
-     * @param array|string $sources
-     * @param string $prefix
-     */
-    public function mapFolderSources(&$sources, $prefix = 'folder:')
-    {
-        if (is_array($sources)) {
-            foreach ($sources as $k => $sourceHandle) {
-                $volume = Craft::$app->volumes->getVolumeByHandle($sourceHandle);
-                $folder = Craft::$app->assets->getRootFolderByVolumeId($volume->id);
-                if ($folder) {
-                    $sources[$k] = $prefix . $folder->id;
-                } else {
-                    unset($sources[$k]);
-                }
-            }
-        } else if (is_string($sources)) {
-            /** @var VolumeInterface $source */
-            $volume = Craft::$app->volumes->getVolumeByHandle($sources);
-            if ($volume) {
-                $folder = Craft::$app->assets->getRootFolderByVolumeId($volume->id);
-            } else {
-                $folder = false;
-            }
-            if ($folder) {
-                $sources = $prefix . $folder->id;
-            } else {
-                $sources = '*';
-            }
-        } else {
-            $sources = '*';
-        }
-    }
-
-    /**
-     * @param array|string $sources
-     * @param string $prefix
-     */
-    public function unmapFolderSources(&$sources, $prefix = 'folder:')
-    {
-        if (is_array($sources)) {
-            foreach ($sources as $k => $sourceRef) {
-                $sourceId = substr($sourceRef, strlen($prefix));
-                $folder = Craft::$app->assets->getFolderById((int) $sourceId);
-                $volume = Craft::$app->volumes->getVolumeById((int) $folder->volumeId);
-                if ($volume) {
-                    $sources[$k] = $volume->handle;
-                } else {
-                    unset($sources[$k]);
-                }
-            }
-        } else if (isset($sources) && $sources !== '*' && $sources !== '') {
-            $sourceId = substr($sources, strlen($prefix));
-            $folder = Craft::$app->assets->getFolderById((int) $sourceId);
-            $volume = Craft::$app->volumes->getVolumeById((int) $folder->volumeId);
-            if ($volume) {
-                $sources = $volume->handle;
-            } else {
-                $sources = '*';
-            }
-        }
-    }
-
-    /**
-     * @param array|string $transforms
-     * @param string $prefix
-     */
-    public function mapAssetTransforms(&$transforms, $prefix = 'transform:')
-    {
-        if (is_array($transforms)) {
-            foreach ($transforms as $k => $transformHandle) {
-                $transform = Craft::$app->assetTransforms->getTransformByHandle($transformHandle);
-                if ($transform) {
-                    $transforms[$k] = $prefix . $transform->id;
-                } else {
-                    unset($transforms[$k]);
-                }
-            }
-        } else if (is_string($transforms)) {
-            $transform = Craft::$app->assetTransforms->getTransformByHandle($transforms);
-            if ($transform) {
-                $transforms = $prefix . $transform->id;
-            } else {
-                $transforms = '*';
-            }
-        } else {
-            $transforms = '*';
-        }
-    }
-
-    /**
-     * @param array|string $transforms
-     * @param string $prefix
-     */
-    public function unmapAssetTransforms(&$transforms, $prefix = 'transform:')
-    {
-        if (is_array($transforms)) {
-            foreach ($transforms as $k => $transformRef) {
-                $transformId = substr($transformRef, strlen($prefix));
-                $transform = Craft::$app->assetTransforms->getTransformById((int) $transformId);
-                if ($transform) {
-                    $transforms[$k] = $transform->handle;
-                } else {
-                    unset($transforms[$k]);
-                }
-            }
-        } else if (is_string($transforms)) {
-            $transformId = substr($transforms, strlen($prefix));
-            $transform = Craft::$app->assetTransforms->getTransformById((int) $transformId);
-            if ($transform) {
-                $transforms = $transform->handle;
-            } else {
-                $transforms = '*';
-            }
-        }
-    }
-
-    /**
-     * @param array|string $sources
-     * @param string $prefix
-     */
-    public function mapSectionSources(&$sources, $prefix = 'section:')
-    {
-        if (is_array($sources)) {
-            foreach ($sources as $k => $sourceHandle) {
-                if ($sourceHandle !== 'singles') {
-                    $source = Craft::$app->sections->getSectionByHandle($sourceHandle);
-                    if ($source->type === 'single') {
-                        $sources[$k] = 'single:' . $source->id;
-                    } else {
-                        $sources[$k] = $prefix . $source->id;
-                    }
-                }
-            }
-        } else if (isset($sources) && $sources !== '*' && $sources !== '' && $sources !== 'singles') {
-            $source = Craft::$app->sections->getSectionByHandle((int) $sources);
-            $sources = $prefix . $source->id;
-        }
-    }
-
-    /**
-     * @param array|string $sources
-     * @param string $prefix
-     */
-    public function unmapSectionSources(&$sources, $prefix = 'section:')
-    {
-        if (is_array($sources)) {
-            foreach ($sources as $k => $sourceRef) {
-                if (substr($sourceRef, 0, strlen('single:')) === 'single:') {
-                    $sourceId = substr($sourceRef, strlen('single:'));
-                    $source = Craft::$app->sections->getSectionById((int) $sourceId);
-                    if ($source) {
-                        $sources[$k] = $source->handle;
-                    } else {
-                        unset($sources[$k]);
-                    }
-                } else if ($sourceRef !== 'singles') {
-                    $sourceId = substr($sourceRef, strlen($prefix));
-                    $source = Craft::$app->sections->getSectionById((int) $sourceId);
-                    if ($source) {
-                        $sources[$k] = $source->handle;
-                    } else {
-                        unset($sources[$k]);
-                    }
-                }
-            }
-        } else if (isset($sources) && $sources !== '*' && $sources !== '' && $sources !== 'singles') {
-            $source = Craft::$app->sections->getSectionById((int) $sources);
-            $sources = $source->handle;
-        }
-    }
-
-    /**
-     * @param array|string $sources
-     * @param string $prefix
-     */
-    public function mapCategorySources(&$sources, $prefix = 'group:')
-    {
-        if (is_array($sources)) {
-            foreach ($sources as $k => $sourceRef) {
-                $categoryGroup = Craft::$app->categories->getGroupByHandle($sourceRef);
-                $sources[$k] = $prefix . $categoryGroup->id;
-            }
-        } else if (isset($sources) && $sources !== '*' && $sources !== '') {
-            $categoryGroup = Craft::$app->categories->getGroupByHandle($sources);
-            $sources = $prefix . $categoryGroup->id;
-        }
-    }
-
-    /**
-     * @param array|string $sources
-     * @param string $prefix
-     */
-    public function unmapCategorySources(&$sources, $prefix = 'group:')
-    {
-        if (is_array($sources)) {
-            foreach ($sources as $k => $sourceRef) {
-                $sourceId = substr($sourceRef, strlen($prefix));
-                $categoryGroup = Craft::$app->categories->getGroupById((int) $sourceId);
-                $sources[$k] = $categoryGroup->handle;
-            }
-        } else if (isset($sources) && $sources !== '*' && $sources !== '') {
-            $sourceId = substr($sources, strlen($prefix));
-            $categoryGroup = Craft::$app->categories->getGroupById((int) $sourceId);
-            $sources = $categoryGroup->handle;
-        }
-    }
-
-    /**
-     * @param array|string $sourceHandle
-     * @param string $prefix
-     */
-    public function mapTagSource(&$sourceHandle, $prefix = 'taggroup:')
-    {
-        $source = Craft::$app->tags->getTagGroupByHandle($sourceHandle);
-        if ($source) {
-            $sourceHandle = $prefix . $source->id;
-        }
-    }
-
-    /**
-     * @param array|string $source
-     * @param string $prefix
-     */
-    public function unmapTagSource(&$source, $prefix = 'taggroup:')
-    {
-        $sourceId = substr($source, strlen($prefix));
-        $tagGroup = Craft::$app->tags->getTagGroupById((int) $sourceId);
-        if ($tagGroup) {
-            $source = $tagGroup->handle;
-        }
-    }
-
-    /**
-     * @param array|string $sources
-     * @param string $prefix
-     */
-    public function mapUserGroupSources(&$sources, $prefix = 'group:')
-    {
-        if (is_array($sources)) {
-            foreach ($sources as $k => $sourceHandle) {
-                if ($sourceHandle !== 'admins') {
-                    $source = Craft::$app->userGroups->getGroupByHandle($sourceHandle);
-                    $sources[$k] = $prefix . $source->id;
-                }
-            }
-        } else {
-            $sources = '*';
-        }
-    }
-
-    /**
-     * @param array|string $sources
-     * @param string $prefix
-     */
-    public function unmapUserGroupSources(&$sources, $prefix = 'group:')
-    {
-        if (is_array($sources)) {
-            foreach ($sources as $k => $sourceRef) {
-                if ($sourceRef !== 'admins') {
-                    $sourceId = substr($sourceRef, strlen($prefix));
-                    $userGroup = Craft::$app->userGroups->getGroupById((int) $sourceId);
-                    $sources[$k] = $userGroup->handle;
-                }
-            }
-        } else {
-            $sources = '*';
-        }
-    }
-
-    /**
      * @param array $item
      */
     public function mapTypeSettings(array &$item)
@@ -750,22 +427,25 @@ abstract class Processor implements ProcessorInterface
                 try {
                     switch ($permissionMap[$permissionName]) {
                         case 'userGroup':
-                            $permissionHandle = Craft::$app->userGroups->getGroupById($permissionId)->handle;
+                            $permissionHandle = Craft::$app->userGroups->getGroupByUid($permissionId)->handle;
                             break;
                         case 'site':
-                            $permissionHandle = Craft::$app->sites->getSiteById($permissionId)->handle;
+                            $permissionHandle = Craft::$app->sites->getSiteByUid($permissionId)->handle;
                             break;
                         case 'section':
-                            $permissionHandle = Craft::$app->sections->getSectionById($permissionId)->handle;
+                            $permissionHandle = Craft::$app->sections->getSectionByUid($permissionId)->handle;
                             break;
                         case 'globalSet':
-                            $permissionHandle = Craft::$app->globals->getSetById($permissionId)->handle;
+                            $permissionHandle = (new Query())
+                                ->from([Table::GLOBALSETS])
+                                ->where(['uid' => $permissionId])
+                                ->one()['handle'];
                             break;
                         case 'category':
-                            $permissionHandle = Craft::$app->categories->getGroupById($permissionId)->handle;
+                            $permissionHandle = Craft::$app->categories->getGroupByUid($permissionId)->handle;
                             break;
                         case 'volume':
-                            $permissionHandle = Craft::$app->volumes->getVolumeById($permissionId)->handle;
+                            $permissionHandle = Craft::$app->volumes->getVolumeByUid($permissionId)->handle;
                             break;
                         default:
                             $permissionHandle = $permissionId;
@@ -788,22 +468,22 @@ abstract class Processor implements ProcessorInterface
                 try {
                     switch ($permissionMap[$permissionName]) {
                         case 'userGroup':
-                            $permissionId = Craft::$app->userGroups->getGroupByHandle($permissionHandle)->id;
+                            $permissionId = Craft::$app->userGroups->getGroupByHandle($permissionHandle)->uid;
                             break;
                         case 'site':
-                            $permissionId = Craft::$app->sites->getSiteByHandle($permissionHandle)->id;
+                            $permissionId = Craft::$app->sites->getSiteByHandle($permissionHandle)->uid;
                             break;
                         case 'section':
-                            $permissionId = Craft::$app->sections->getSectionByHandle($permissionHandle)->id;
+                            $permissionId = Craft::$app->sections->getSectionByHandle($permissionHandle)->uid;
                             break;
                         case 'globalSet':
-                            $permissionId = Craft::$app->globals->getSetByHandle($permissionHandle)->id;
+                            $permissionId = Craft::$app->globals->getSetByHandle($permissionHandle)->uid;
                             break;
                         case 'category':
-                            $permissionId = Craft::$app->categories->getGroupByHandle($permissionHandle)->id;
+                            $permissionId = Craft::$app->categories->getGroupByHandle($permissionHandle)->uid;
                             break;
                         case 'volume':
-                            $permissionId = Craft::$app->volumes->getVolumeByHandle($permissionHandle)->id;
+                            $permissionId = Craft::$app->volumes->getVolumeByHandle($permissionHandle)->uid;
                             break;
                         default:
                             $permissionId = $permissionHandle;
